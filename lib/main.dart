e72599627fb7a8a66ae0968cfa935046d085fae6
Byeong Gui Jeong <jsmile@naver.com>
2023-08-02 14:42:11 +0900

    basic main ui and basic ColorLoc & CounerBloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'blocs/color/color_bloc.dart';
+import 'blocs/counter/counter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,12 +13,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Bloc to Bloc Communication',
-      theme: ThemeData(
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ColorBloc>(
+          create: (context) => ColorBloc(),
+        ),
+        BlocProvider<CounterBloc>(
+          create: (context) => CounterBloc(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Bloc to Bloc Communication',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'Flutter Bloc2Bloc Home Page'),
       ),
-      home: const MyHomePage(title: 'Flutter Bloc2Bloc Home Page'),
     );
   }
 }
@@ -27,6 +42,7 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.red,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(title),
@@ -35,13 +51,35 @@ class MyHomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            ElevatedButton(
+              child: const Text(
+                'Change Color',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                ),
+              ),
+              onPressed: () {},
+            ),
+            const SizedBox(height: 20.0),
             const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
               '0',
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 52.0,
+              ),
             ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              child: const Text(
+                'Change Counter',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                ),
+              ),
+              onPressed: () {},
+            )
           ],
         ),
       ),
